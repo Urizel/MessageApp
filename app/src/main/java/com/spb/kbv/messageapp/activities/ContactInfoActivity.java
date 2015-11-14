cd3fade5bd8055e215c1d5aa31cbd71dfb6668e6
klimovbv <bogdan.zp@mail.ru>
2015-11-14 23:57:05 +0300

    Filling ContactInfoActivity(2)

--- app/src/main/java/com/spb/kbv/messageapp/activities/ContactInfoActivity.java
+++ app/src/main/java/com/spb/kbv/messageapp/activities/ContactInfoActivity.java
@@ -1,22 +1,29 @@
 package com.spb.kbv.messageapp.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.spb.kbv.messageapp.R;
+import com.spb.kbv.messageapp.services.Contacts;
 import com.spb.kbv.messageapp.services.Messages;
 import com.spb.kbv.messageapp.services.entities.Message;
 import com.spb.kbv.messageapp.services.entities.UserDetails;
 import com.spb.kbv.messageapp.views.MessagesAdapter;
+import com.squareup.otto.Subscribe;
 
 import java.util.ArrayList;
 
 public class ContactInfoActivity extends BaseAuthenticatedActivity implements MessagesAdapter.OnMessageClickListener {
     public static final String EXTRA_USER_DETAILS = "EXTRA_USER_DETAILS";
+    public static final int RESULT_USER_REMOVED = 101;
 
     private UserDetails userDetails;
     private MessagesAdapter adapter;
@@ -27,23 +34,11 @@ public class ContactInfoActivity extends BaseAuthenticatedActivity implements Me
     protected void onMessageAppCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_contacts_info);
 
-        progressFrame = findViewById(R.id.activity_contacts_info_progressFrame);
         userDetails = getIntent().getParcelableExtra(EXTRA_USER_DETAILS);
-        if (userDetails == null){
+        if (userDetails.getDisplayName() == null){
             userDetails = new UserDetails(1, true, "A contact", "a_contact", "http://www.gravatar.com/avatar/1.jpg");
         }
 
-        adapter = new MessagesAdapter(this, this);
-        messages = adapter.getMessages();
-
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.activity_contacts_info_messages);
-        if  (isTablet){
-            recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        } else {
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        }
-
-        recyclerView.setAdapter(adapter);
         getSupportActionBar().setTitle(userDetails.getDisplayName());
         toolbar.setNavigationIcon(R.drawable.ic_ab_close);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -53,9 +48,21 @@ public class ContactInfoActivity extends BaseAuthenticatedActivity implements Me
             }
         });
 
+        adapter = new MessagesAdapter(this, this);
+        messages = adapter.getMessages();
+
+        progressFrame = findViewById(R.id.activity_contacts_info_progressFrame);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.activity_contacts_info_messages);
+        if  (isTablet){
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        } else {
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }
+
+        recyclerView.setAdapter(adapter);
+
         scheduler.postEveryMilliseconds(new Messages.SearchMessagesRequest(userDetails.getId(), true, true),1000 * 60 * 3);
-
-
     }
 
     @Override
@@ -63,5 +70,80 @@ public class ContactInfoActivity extends BaseAuthenticatedActivity implements Me
         Intent intent = new Intent(this, MessageActivity.class);
         intent.putExtra(MessageActivity.EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Subscribe
+    public void onMessagesReceived(final Messages.SearchMessageResponse respones){
+        scheduler.invokeOnResume(Messages.SearchMessageResponse.class, new Runnable() {
+            @Override
+            public void run() {
+                if (!respones.didSucceed()) {
+                    respones.showErrorToast(ContactInfoActivity.this);
+                    return;
+                }
+
+                int oldSize = messages.size();
+                messages.clear();
+                adapter.notifyItemRangeRemoved(0, oldSize);
+
+                messages.addAll(respones.messages);
+                adapter.notifyItemRangeInserted(0, messages.size());
+            }
+        });
+        progressFrame.setVisibility(View.GONE);
+    }
+    private void doRemoveContact(){
+        progressFrame.setVisibility(View.VISIBLE);
+        bus.post(new Contacts.RemoveContactRequest(userDetails.getId()));
+    }
+
+    @Subscribe
+    public void onRemoveContact(final Contacts.RemoveContactResponse response){
+        scheduler.invokeOnResume(Contacts.RemoveContactResponse.class, new Runnable() {
+            @Override
+            public void run() {
+                if (!response.didSucceed()) {
+                    response.showErrorToast(ContactInfoActivity.this);
+                    progressFrame.setVisibility(View.VISIBLE);
+                    return;
+                }
+                setResult(RESULT_USER_REMOVED);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_contacts_info, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.activity_contact_info_menuNewMessage) {
+            Intent intent = new Intent(this, NewMessageActivity.class);
+            intent.putExtra(NewMessageActivity.EXTRA_CONTACT, userDetails);
+            startActivity(intent);
+            return true;
+        }
+
+        if (itemId == R.id.activity_contact_info_menuRemoveFriend) {
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Remove Friend")
+                    .setPositiveButton("Remove", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            doRemoveContact();
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .create();
+            dialog.show();
+            return true;
+        }
+
+        return false;
     }
 }
