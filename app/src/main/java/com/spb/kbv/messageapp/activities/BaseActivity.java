72049089733c77481c313dbb320e6df8fb3c8340
klimovbv <bogdan.zp@mail.ru>
2015-11-19 17:31:14 +0300

    Implemented SwipeRefresh to MainActivity

--- app/src/main/java/com/spb/kbv/messageapp/activities/BaseActivity.java
+++ app/src/main/java/com/spb/kbv/messageapp/activities/BaseActivity.java
@@ -1,7 +1,9 @@
 package com.spb.kbv.messageapp.activities;
 
 import android.animation.Animator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
@@ -13,7 +15,7 @@ import com.spb.kbv.messageapp.infrastructure.MessageApplication;
 import com.spb.kbv.messageapp.views.NavDrawer;
 import com.squareup.otto.Bus;
 
-public class BaseActivity extends ActionBarActivity {
+public class BaseActivity extends ActionBarActivity implements SwipeRefreshLayout.OnRefreshListener {
     private boolean isRegisteredWithBus;
 
     protected MessageApplication application;
@@ -22,6 +24,7 @@ public class BaseActivity extends ActionBarActivity {
     protected boolean isTablet;
     protected Bus bus;
     protected ActionScheduler scheduler;
+    protected SwipeRefreshLayout swipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +84,17 @@ public class BaseActivity extends ActionBarActivity {
         if (toolbar != null){
             setSupportActionBar(toolbar);
         }
+
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh);
+        if (swipeRefresh != null){
+            swipeRefresh.setOnRefreshListener(this);
+        }
+        swipeRefresh.setColorSchemeColors(
+                Color.parseColor("#FF00DDFF"),
+                Color.parseColor("#FF99CC00"),
+                Color.parseColor("#FFFFBB33"),
+                Color.parseColor("#FFFF4444")
+        );
     }
 
     public void fadeOut(final FadeOutListener listener){
@@ -110,6 +124,11 @@ public class BaseActivity extends ActionBarActivity {
                 })
                 .setDuration(300)
                 .start();
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 
     public interface FadeOutListener {
